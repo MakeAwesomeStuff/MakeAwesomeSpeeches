30eb7b0876f33518d30c3dfb6808a6ed04ac3766
David Langford <langford.d@gmail.com>
2015-02-02 08:47:56 +1000

    Each Facebook user can now save 1 speech

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -8,6 +8,11 @@
  * Controller of the makeawesomespeechesApp
  */
 angular.module('makeawesomespeechesApp')
-  .controller('AccountCtrl', ['$scope', 'user', function ($scope, user) {
+  .controller('AccountCtrl', ['$scope', 'fbutil', 'user',
+                    function ($scope, fbutil, user) {
     $scope.user = user;
+    // download the data into a local object
+    var syncObject = fbutil.syncObject('speeches/'+user.uid+'/firstspeechid');
+    // synchronize the object with a three-way data binding
+    syncObject.$bindTo($scope, 'currentSpeech');
   }]);
