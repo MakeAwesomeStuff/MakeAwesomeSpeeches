a7ad8f320726e0b7d9b879f27334d25e2769eedd
David Langford <langford.d@gmail.com>
2015-02-09 21:55:24 +1000

    Creating a new speech will load that speech for the user to edit

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -8,27 +8,51 @@
  * Controller of the makeawesomespeechesApp
  */
 angular.module('makeawesomespeechesApp')
-  .controller('AccountCtrl', ['$scope', 'fbutil', 'user',
-                    function ($scope, fbutil, user) {
+  .controller('AccountCtrl', ['$scope', '$location', 'fbutil', 'user',
+                    function ($scope, $location, fbutil, user) {
     $scope.user = user;
-    // download the data into a local object
-    var syncObject = fbutil.syncObject('speeches/'+user.uid+'/firstspeechid');
 
-    // synchronize the object with a three-way data binding
-    syncObject.$bindTo($scope, 'currentSpeech');
+    // Get list of stories and use that to find the latest
+    // Note: Remember not to use push(), splice(), etc on this 'array'
+    $scope.speechList = fbutil.syncArray('userSpeechesList/'+user.uid);
+    $scope.speechList.$loaded(function() {
+      if ($scope.speechList.length === 0) {
+        $scope.createSpeech();
+      } else {
+        $scope.syncLatestSpeech();
+      }
+    });
 
-    $scope.newSpeech = function() {
-      var newSpeech = {};
-      newSpeech.createdOn = new Date().toJSON();
-      newSpeech.content = 'Write your new speech here :)';
-
-      // Push directly onto the object
-      var key = fbutil.pushObject('speeches/'+user.uid, newSpeech);
-      // console.log('key:', key);
-      // Need to add to speeches list
-      var newSpeechListItem = {};
-      newSpeechListItem.key = key;
-      newSpeechListItem.createdOn = newSpeech.createdOn;
-      fbutil.pushObject('userSpeechesList/'+user.uid, newSpeechListItem);
+    $scope.syncLatestSpeech = function() {
+      var storyKey = $scope.speechList[$scope.speechList.length-1].key;
+      $scope.syncSpeech(storyKey);
     };
+
+    $scope.syncSpeech = function(storyKey) {
+      if ($scope.syncedObject) {
+        // Otherwise we overwrite the previous one if not a hard refresh
+        $scope.syncedObject.$destroy();
+      }
+      $scope.syncedObject = fbutil.syncObject('speeches/'+user.uid+'/'+storyKey);
+      $scope.syncedObject.$bindTo($scope, 'currentSpeech');
+    };
+
+    $scope.createSpeech = function() {
+      var newSpeech = {
+        content: 'Brand new speech!',
+        createdOn: new Date().toJSON(),
+        title: 'Speech ' + ($scope.speechList.length+1)
+      };
+      fbutil.pushObject('speeches/'+user.uid, newSpeech).then(function(ref) {
+        var newSpeechListItem = {
+            createdOn: newSpeech.createdOn,
+            key: ref.key(),
+            title: newSpeech.title
+        };
+        $scope.speechList.$add(newSpeechListItem).then(function() {
+          $scope.syncLatestSpeech();
+        });
+      });
+    };
+
   }]);
